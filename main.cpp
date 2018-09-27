efcf1c617c95f31924c73ac075a601c2d31bfa1d
Ahmed Helal Ahmed <ahmed.helal.online@gmail.com>
2018-09-27 23:30:45 +0200

    make code optimized

--- main.cpp
+++ main.cpp
@@ -5,18 +5,7 @@ using namespace std;
 void f(int n);
 int main()
 {
-    /*
-    //optimze #1 and #2
-    int n;
-    while(n)
-    {
-        cout<<"Enter n to draw Z letter : ";
-        cin>>n;
-        f(n);
-    }
 
-    */
-    // optimze #3
     int n;
     cout<<"Enter n to draw Z letter : ";
     cin>>n;
@@ -24,7 +13,7 @@ int main()
     return 0;
 }
 
-//optimze #3
+
 void f(int n)
 {
     int flag = 1;
@@ -68,109 +57,3 @@ void f(int n)
 
 
 
-
-
-
-// optimze #2
-/*
-void f(int n)
-{
-    int flag = 1;
-    int j=0;
-    int counter = 0 ;
-    int k=0;
-for(int i=1; i<=n; i++)
-{
-    k++;
-    if(i==1||n+1-i == k||i==n)
-    {
-        cout<<'*';
-    }
-    else
-    {
-        cout<<' ';
-    }
-    if( k==n)
-    {
-        cout<<endl;
-        k=0;
-    }
-    else
-    {
-        i--;
-    }
-
-
-
-}
-
-    }
-
-       */
-
-
-
-
-//optimize #1
-/*
-void f(int n)
-{
-    for(int i=1; i<=n; i++)
-    {
-        for(int j=1; j<=n; j++)
-        {
-
-            if(n+1-i == j||i==1||i==n)
-            {
-                cout<<'*';
-            }
-            else
-            {
-                cout<<' ';
-            }
-
-        }
-    cout<<endl;
-    }
-
-
-}
-
-*/
-/*
-void f(int n)
-{
-    int counter=0;
-    for(int i=0; i<=n; i++)
-    {
-        counter=0;
-
-        for(int k=1; k<n-i; k++)
-        {
-            if(i==0)break;
-            counter++;
-            cout<<" ";
-        }
-        for(int j=0; j<n-i; j++)
-        {
-
-            if(counter<n)
-            {
-                cout<<"*";
-                counter++;
-
-            }
-            if(i>0)
-            {
-                break;
-            }
-        }
-
-        for(int l=n-1; l>0&&i==n-1; l--)
-        {
-            cout<<'*';
-        }
-        cout<<endl;
-    }
-}
-*/
