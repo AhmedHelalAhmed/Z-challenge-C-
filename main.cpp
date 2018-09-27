576d862084ab56b4c0b12381fdd3bff589910f12
Ahmed Helal Ahmed <ahmed.helal.online@gmail.com>
2018-09-27 21:44:28 +0200

    add optimze #1, #2 and #3

--- main.cpp
+++ main.cpp
@@ -5,6 +5,8 @@ using namespace std;
 void f(int n);
 int main()
 {
+    /*
+    //optimze #1 and #2
     int n;
     while(n)
     {
@@ -12,9 +14,130 @@ int main()
         cin>>n;
         f(n);
     }
+
+    */
+    // optimze #3
+    int n;
+    cout<<"Enter n to draw Z letter : ";
+    cin>>n;
+    f(n);
     return 0;
 }
 
+//optimze #3
+void f(int n)
+{
+    int flag = 1;
+    int j=0;
+
+    for(int i=1; i<=n; i++)
+    {
+        j++;
+        if(i==1||n+1-i == j||i==n)
+        {
+            cout<<'*';
+        }
+        else
+        {
+            cout<<' ';
+        }
+        if( j==n)
+        {
+            cout<<endl;
+            j=0;
+        }
+        else
+        {
+            i--;
+        }
+
+        if(i==n)
+        {
+            cout<<"Enter n to draw Z letter : ";
+            cin>>n;
+            if(n)
+            {
+                i=0;
+            }
+        }
+
+
+    }
+
+}
+
+
+
+
+
+
+// optimze #2
+/*
+void f(int n)
+{
+    int flag = 1;
+    int j=0;
+    int counter = 0 ;
+    int k=0;
+for(int i=1; i<=n; i++)
+{
+    k++;
+    if(i==1||n+1-i == k||i==n)
+    {
+        cout<<'*';
+    }
+    else
+    {
+        cout<<' ';
+    }
+    if( k==n)
+    {
+        cout<<endl;
+        k=0;
+    }
+    else
+    {
+        i--;
+    }
+
+
+
+}
+
+    }
+
+       */
+
+
+
+
+//optimize #1
+/*
+void f(int n)
+{
+    for(int i=1; i<=n; i++)
+    {
+        for(int j=1; j<=n; j++)
+        {
+
+            if(n+1-i == j||i==1||i==n)
+            {
+                cout<<'*';
+            }
+            else
+            {
+                cout<<' ';
+            }
+
+        }
+    cout<<endl;
+    }
+
+
+}
+
+*/
+/*
 void f(int n)
 {
     int counter=0;
@@ -50,3 +173,4 @@ void f(int n)
         cout<<endl;
     }
 }
+*/
